9ed5a04e55287f2acd8ff2eb9bda05ac9ab53677
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2025-03-18 13:25:33 +0530

    feat(eleven-labs): ran and tested

--- lib/init-eleven-labs.ts
+++ lib/init-eleven-labs.ts
@@ -1,6 +1,13 @@
 import { ElevenLabsClient } from "elevenlabs";
 
-const elevenlabs = new ElevenLabsClient();
+const apiKey = process.env.ELEVENLABS_API_KEY;
+if (!apiKey) {
+  throw new Error("Please pass in your ElevenLabs API Key or export ELEVENLABS_API_KEY in your environment.");
+}
+
+const elevenlabs = new ElevenLabsClient({
+  apiKey,
+});
 
 export default elevenlabs;
 
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY,
+  },
 };
 
 export default nextConfig;
